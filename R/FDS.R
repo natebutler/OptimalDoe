80cbaef28ab0b140684643686faff74749a12b15
natebutler <nathan.butler@usu.edu>
2024-04-17 19:01:59 -0600

    Adding FDS plots

--- R/FDS.R
+++ R/FDS.R
@@ -1,5 +1,6 @@
-RPV <- function(Design, k, order) {
+RPV <- function(Design, order) {
   pv <- rep(0, 10000)
+  k <- ncol(Design)
   f <- model_matrix(Design, order)
   for (i in seq_len(10000)) {
     X_samp <- matrix(runif(k, min = -1, max = 1), nrow = 1)
@@ -10,15 +11,31 @@ RPV <- function(Design, k, order) {
   return (pv)
 }
 
-FDS_helper <- function(Des_list, N, k) {
-  RPV_df <- data.frame()
+FDS_helper <- function(Des_list, order) {
+  RPV_df <- matrix(nrow = 10000, ncol = length(Des_list))
   for (i in seq_len(length(Des_list))) {
-    RPV_df[,i] <- RPV(Des_list[[i]])
-    colnames(RPV_df)[i] <- paste0("Design", i)
+    RPV_df[,i] <- sort(RPV(Des_list[[i]], order))
   }
+  RPV_df <- data.frame(RPV_df)
+  colnames(RPV_df) <- paste0("Design", seq_along(Des_list))
   return (RPV_df)
 }
 
 
-ju <- matrix(c(1,0,-1,0.2), nrow = 1)
-ju
+# des_list_check <- lapply(head(sorted, 5), `[[`, 1)
+
+FDS_plot <- function(data) {
+  y_vars <- names(data)
+
+  # Create a ggplot object
+  p <- ggplot(data, aes(x=c(1:10000)/10000))
+
+  # Add a geom_line for each y variable
+  colors <- rainbow(length(y_vars))
+  for (i in seq_along(y_vars)) {
+    p <- p + geom_line(aes(y = !!sym(y_vars[i])), color = colors[i])
+  }
+
+  # Return the plot
+  return(p)
+}
