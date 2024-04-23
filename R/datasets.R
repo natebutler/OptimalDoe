049118c63d7e99a3bebefbd34517852d7da2448d
Tommy Bolton <tommithan14@gmail.com>
2024-04-23 16:20:39 -0600

    adding documentation to the included datasets

---
+++ R/datasets.R
@@ -0,0 +1,25 @@
+#' optimal_A_14_3
+#'
+#' A list of generated A-optimal experimental designs.
+#'
+#' @format ## `optimal_A_14_3`
+#' A list with 1000 entries, each entry is a list with a length of 2:
+#' \describe{
+#'   \item{entry 1}{Locally A-optimal design matrix}
+#'   \item{entry 2}{The associated score with the design matrix}
+#'   ...
+#' }
+"optimal_A_14_3"
+
+#' optimal_D_14_3
+#'
+#' A list of generated D-optimal experimental designs.
+#'
+#' @format ## `optimal_A_14_3`
+#' A list with 1000 entries, each entry is a list with a length of 2:
+#' \describe{
+#'   \item{entry 1}{Locally D-optimal design matrix}
+#'   \item{entry 2}{The associated score with the design matrix}
+#'   ...
+#' }
+"optimal_D_14_3"
