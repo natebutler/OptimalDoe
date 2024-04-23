a33ab61063f94cf9726e2d2f8bec3860165f848b
Tommy Bolton <tommithan14@gmail.com>
2024-04-22 19:37:14 -0600

    addin documentation to singular_check function

--- R/scorings.R
+++ R/scorings.R
@@ -78,8 +78,19 @@ A_crit <- function(design, order){
   }
   return(crit)
 }
-
 # this function checks if the design matrix is a singular matrix
+#' Check Singularity of an information matrix from a design matrix.
+#'
+#' This function takes in a design matrix and a model desired to be fit. From
+#' this, we expand the design out into the model matrix and then compute the
+#' information matrix. This function then checks if the matrix is invertible.
+#'
+#' @param X A design matrix with n rows and k columns.
+#' @param order Model you want to fit. 0 fits a first order model. 1 fits a
+#' first order model with two-way interactions. 2 fits a second order model.
+#'
+#' @returns This function returns a TRUE if the information matrix created from
+#' the design matrix is singular, FALSE if not.
 #' @export
 singular_check <- function(X, order){
   f <- model_matrix(X, order)
