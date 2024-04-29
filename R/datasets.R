549a5e32700168f2bbef8f9993efe85751937301
Tommy Bolton <tommithan14@gmail.com>
2024-04-28 21:26:20 -0600

    changed the generated designs for FDS plots and added more interpretation to the code blocks

--- R/datasets.R
+++ R/datasets.R
@@ -2,24 +2,24 @@
 #'
 #' A list of generated A-optimal experimental designs.
 #'
-#' @format ## `optimal_A_14_3`
-#' A list with 1000 entries, each entry is a list with a length of 2:
+#' @format ## `optimal_A_25_4`
+#' A list with 100 entries, each entry is a list with a length of 2:
 #' \describe{
-#'   \item{entry 1}{Locally A-optimal design matrix}
+#'   \item{entry 1}{Locally A-optimal design matrix with 25 rows and 4 columns}
 #'   \item{entry 2}{The associated score with the design matrix}
 #'   ...
 #' }
-"optimal_A_14_3"
+"optimal_A_25_4"
 
-#' optimal_D_14_3
+#' optimal_D_25_4
 #'
 #' A list of generated D-optimal experimental designs.
 #'
-#' @format ## `optimal_A_14_3`
-#' A list with 1000 entries, each entry is a list with a length of 2:
+#' @format ## `optimal_D_25_4`
+#' A list with 100 entries, each entry is a list with a length of 2:
 #' \describe{
-#'   \item{entry 1}{Locally D-optimal design matrix}
+#'   \item{entry 1}{Locally D-optimal design matrix with 25 rows and 4 columns}
 #'   \item{entry 2}{The associated score with the design matrix}
 #'   ...
 #' }
-"optimal_D_14_3"
+"optimal_D_25_4"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,11 +2,11 @@
 
 set.seed(3657)
 
-optimal_A_14_3 <- generate_designs(14, 3, 1, "A", 1000)
-result_A_14_3 <- lapply(head(optimal_A_14_3, 4), `[[`, 1)
+optimal_A_25_4 <- generate_designs(25, 4, 2, "A", 100)
+result_A_25_4 <- lapply(head(optimal_A_25_4, 4), `[[`, 1)
 
-optimal_D_14_3 <- generate_designs(14, 3, 1, "D", 1000)
-result_D_14_3 <- lapply(head(optimal_D_14_3, 4), `[[`, 1)
+optimal_D_25_4 <- generate_designs(25, 4, 2, "D", 100)
+result_D_25_4 <- lapply(head(optimal_D_25_4, 4), `[[`, 1)
 
-usethis::use_data(optimal_D_14_3, overwrite = TRUE)
-usethis::use_data(optimal_A_14_3, overwrite = TRUE)
+usethis::use_data(optimal_D_25_4, overwrite = TRUE)
+usethis::use_data(optimal_A_25_4, overwrite = TRUE)
